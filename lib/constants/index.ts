1aa44dae6721a05e807b6a987392fa9fe09a24fc
Emmanuel <nkongolo64@gmail.com>
2025-07-12 19:13:07 +0200

    Load product from database

--- lib/constants/index.ts
+++ lib/constants/index.ts
@@ -5,3 +5,5 @@ export const APP_DESCRIPTION =
 
 export const SERVER_URL =
   process.env.NEXT_PUBLIC_SERVER_URL || "http://localhost:3000";
+export const LATES_PRODUCTS_LIMIT =
+  Number(process.env.LATES_PRODUCTS_LIMIT) || 4;
